fc51e5540eb9645d676effd55742fe07118fed24
Tanmay Chauk <tanmay.chauk@agiliad.com>
2025-09-26 00:56:29 +0530

    BLE script invoking added from run_all script, config file added for basic config

--- run_all.sh
+++ run_all.sh
@@ -13,4 +13,7 @@ python3 nfc_service.py > nfc_service.log 2>&1 &
 # source /path/to/venv/bin/activate
 ./can_start.sh > can_j1939_service.log 2>&1 &
 
+source opsky_ble/venv/bin/activate
+python3 opsky_ble/opsky_simulator.py > opsky_ble/opsky_simulator.log
+
 wait
